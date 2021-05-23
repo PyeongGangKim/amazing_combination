b7b93faabf90a074d8ed5697b669e6ebbe996a47
laz <21600293@handong.edu>
2021-05-23 23:56:01 +0900

    feat: 홈, 브랜드, 검색 페이지 생성

---
+++ lib/Home.dart
@@ -0,0 +1,45 @@
+
+import 'package:amazing_combination/Live.dart';
+import 'package:amazing_combination/widgets/combinationList.dart';
+import 'package:amazing_combination/widgets/bottomNavigator.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("홈"),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: <Widget>[
+          TagButton('치킨'),
+          TagButton('햄버거'),
+          TagButton('피자'),
+          TagButton('중식'),
+        ],
+      ),
+    );
+  }
+}
+
+Widget TagButton(String tag) {
+  // TODO: onPressed effect
+  return Column(
+    children: <Widget>[
+      IconButton(
+        iconSize: 80,
+        icon: Icon(Icons.fastfood),
+        onPressed: () {
+          // navigate
+          print(tag);
+        },
+      ),
+      Text(tag),
+    ],
+  );
+}
+
